58cf2b50b60a9ee43aa4a7d370821ad1d4bc173a
Daisy Guo <guoyingc@cn.ibm.com>
2018-10-25 18:28:24 +0800

    add model files

--- mobilenet/downloadmodels.sh
+++ mobilenet/downloadmodels.sh
@@ -2,7 +2,7 @@
 
 cat model.json | jq -r ".weightsManifest[].paths[0]" | sed 's/^/https:\/\/storage.googleapis.com\/tfjs-models\/tfjs\/mobilenet_v1_1.0_224\//' >> temp.out
 
-for x in $(cat temp.out)
+for url in $(cat temp.out)
 do
     echo "fetching $url"
     curl $url -O -s &
